cb2d32aa6a9ed1bb06aabd492bcc06b64fd5ecba
Alex Shevchenko <dozory4@gmail.com>
2021-11-25 16:24:34 +0500

    TT-5: Add tests for the method getResultArithmeticOperation
    1) Change the name method MathFunctional on the getResultArithmeticOperation
    
    2) Add test for method getResultArithmeticOperation

--- src/main/java/com/byndyusoft/util/MathUtil.java
+++ src/main/java/com/byndyusoft/util/MathUtil.java
@@ -5,6 +5,28 @@ public class MathUtil {
     private MathUtil() {
     }
 
+    public static double getResultArithmeticOperation(double a, double b, String operator) throws IllegalArgumentException {
+        double result;
+
+        switch (operator) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                throw new IllegalArgumentException("The operator is not supported: " + operator);
+        }
+        return result;
+    }
+
     public static boolean isNumeric(String str) {
         try {
             Double.parseDouble(str);
