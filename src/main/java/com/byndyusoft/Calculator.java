bf33432da75c2e63e5ecc866e875a4b28918b007
Alex Shevchenko <dozory4@gmail.com>
2021-11-26 17:06:00 +0500

    TT-8: add comments

--- src/main/java/com/byndyusoft/Calculator.java
+++ src/main/java/com/byndyusoft/Calculator.java
@@ -3,7 +3,11 @@ package com.byndyusoft;
 public interface Calculator {
 
 
-
-
+    /**
+     * Return result specified math expression
+     *
+     * @param expression - the string for calculate
+     * @return the double value result represented by the string argument.
+     */
     double calculate(String expression);
 }
